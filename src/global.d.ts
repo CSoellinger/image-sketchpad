d7f7c5b53dd04903c17eb24af29f35013f762188
CSoellinger <zerogiven@gmail.com>
2021-12-02 02:18:31 +0100

    Remove file save browser from type declaration

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,8 +2,3 @@ declare module 'draw-to-canvas' {
   function drawToCanvas(canvasOne: HTMLCanvasElement, canvasTwo: HTMLCanvasElement, pos?: number[]): void;
   export = drawToCanvas;
 };
-
-declare module 'file-save-browser' {
-  function FileDownload(base64: string, type: string, name: string): Promise<void>;
-  export = FileDownload;
-}
